bf0d885f0db51ae45f68f00861342ae11088d9a0
gero zayas <gerozayas@gmail.com>
2023-07-26 11:18:14 +0200

    Initial

---
+++ main.py
@@ -0,0 +1,45 @@
+import PySimpleGUI as sg
+import random
+
+def read_lines_from_file(filename):
+    with open(filename, "r") as file:
+        lines = file.readlines()
+    return lines
+
+def main():
+    sg.theme("DarkAmber")  # Optional: Set the PySimpleGUI theme
+
+    # Define the custom font file path
+    font_size = 18  # Adjust the font size as per your preference
+
+    # Set the font for the window and elements
+    font = ('Arial', font_size)
+
+    # Use the font in the layout
+    layout = [
+        [sg.Button("Browse"), sg.Button("Show Random Line")],
+        [sg.Text(size=(80, 5), key="-OUTPUT-", font=font)],
+    ]
+
+    window = sg.Window("Random Line Viewer", layout)
+
+    while True:
+        event, values = window.read()
+
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == "Browse":
+            file_path = sg.popup_get_file("Select a text file", font=font)
+            if file_path:
+                lines = read_lines_from_file(file_path)
+        elif event == "Show Random Line":
+            if lines:
+                random_line = random.choice(lines).strip()
+                window["-OUTPUT-"].update(random_line)
+            else:
+                window["-OUTPUT-"].update("No lines in the file.")
+
+    window.close()
+
+if __name__ == "__main__":
+    main()
